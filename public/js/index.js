f5b894292027812bf6c75b411c5b763b3cecd783
Abdulkadir98 <ak98.narutokun@gmail.com>
2017-08-26 20:12:40 +0530

    Add css styling for chat

---
+++ public/js/index.js
@@ -0,0 +1,73 @@
+var socket = io();
+    socket.on('connect', function () {
+        console.log('Connected with server');
+        // socket.emit('createEmail', {
+        //     to:'khala97@fag.com',
+        //     text:'sup'
+        // });
+
+        // socket.emit('createMessage', {
+        //     to:'someFag',
+        //     text: 'sup dawg'
+        // });
+    });
+
+    socket.on('disconnect' , function () {
+        console.log('Disconnected from server');
+    });
+
+    // // socket.on('newEmail', function (email){
+    // //     console.log('new Email',email);
+    // });
+socket.on('newMessage', function (message){
+    console.log('new message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}:`);
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+
+});
+
+jQuery('#message-form').on('submit', function(e) {
+
+    var messageTextBox = jQuery('[name=message]');
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from:'User',
+        text: messageTextBox.val()
+    }, function() {
+        messageTextBox.val('')
+    });
+});
+
+var locationButton = jQuery('#send-location');
+
+
+locationButton.on('click', function() {
+    if(!navigator.geolocation){
+        return alert('Gelocation not supported in your browser');
+    }
+
+    locationButton.attr('disabled','disabled');
+    navigator.geolocation.getCurrentPosition(function (position) {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+
+        locationButton.removeAttr('disabled');
+    }, function() {
+        alert('Unable to fetch location');
+        locationButton.removeAttr('disabled');
+    });
+
+});
